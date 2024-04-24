db921546806b09ff9074dacfb2b46444b5d61975
BohdanRadchenko <bradchenko@gmail.com>
2024-04-24 17:19:30 +0300

    feat(pros-cons): added pros/cons shortcode 1

--- shortcodes/cons-shortcode-1.php
+++ shortcodes/cons-shortcode-1.php
@@ -1,65 +1,60 @@
 <?php
 
-function aces_cons_shortcode_1( $atts, $content = null ) {
+function aces_cons_shortcode_1($atts, $content = null)
+{
 
-	ob_start();
+    ob_start();
 
-	// Define attributes and their defaults
+    // Define attributes and their defaults
 
-	extract(
-		shortcode_atts(
-			array (
-			    'title' => '',
-			),
-			$atts
-		)
-	);
+    extract(
+        shortcode_atts(
+            array(
+                'title' => '',
+            ),
+            $atts
+        )
+    );
 
-	$allowed_html = array(
-		'a' => array(
-			'href' => true,
-			'title' => true,
-			'target' => true,
-			'rel' => true
-		),
-		'img' => array(
-			'src' => true,
-			'alt' => true
-		),
-		'br' => array(),
-		'em' => array(),
-		'strong' => array(),
-		'span' => array(
-			'class' => true,
-			'style' => true
-		),
-		'div' => array(
-			'class' => true
-		),
-		'p' => array(),
-		'ul' => array(),
-		'ol' => array(),
-		'li' => array(),
-	);
+    $allowed_html = array(
+        'a' => array(
+            'href' => true,
+            'title' => true,
+            'target' => true,
+            'rel' => true
+        ),
+        'img' => array(
+            'src' => true,
+            'alt' => true
+        ),
+        'br' => array(),
+        'em' => array(),
+        'strong' => array(),
+        'span' => array(
+            'class' => true,
+            'style' => true
+        ),
+        'div' => array(
+            'class' => true
+        ),
+        'p' => array(),
+        'ul' => array(),
+        'ol' => array(),
+        'li' => array(),
+    );
 
-	?>
+    ?>
 
-	<div class="space-cons box-100 relative">
-		<div class="space-cons-ins relative">
-			<div class="space-cons-title box-100 relative">
-				<?php echo esc_html( $title ); ?>
-			</div>
-			<div class="space-cons-description box-100 relative">
-				<?php echo wp_kses( $content, $allowed_html ); ?>
-			</div>
-		</div>
-	</div>
+    <div class="benefits benefits_clubs benefits_negative">
+        <div class="benefits__title"><?php echo esc_html($title); ?></div>
+        <?php echo wp_kses($content, $allowed_html); ?>
+    </div>
 
-	<?php
+    <?php
 
-	$aces_taxonomy_items = ob_get_clean();
-	return $aces_taxonomy_items;
+    $aces_taxonomy_items = ob_get_clean();
+    return $aces_taxonomy_items;
 
 }
- 
+
 add_shortcode('aces-cons-1', 'aces_cons_shortcode_1');
